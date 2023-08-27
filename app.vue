7002590af703a407e5b29dcc2acf95740fa1a1d9
Misbah Ansori <misbah.ansori24@gmail.com>
2023-08-27 10:19:46 +0700

    wip

--- app.vue
+++ app.vue
@@ -1,6 +1,51 @@
 <template>
-  <div class="bg-[#F3F3F3] min-h-screen text-gray-900 font-sans">
-    <Navbar />
-    <Hero />
+  <div class="bg-[#F3F3F3] text-gray-900 font-sans">
+    <section class="min-h-screen flex flex-col">
+      <nav class="py-8 fixed top-0 inset-x-0 bg-white z-50">
+        <div class="max-w-4xl mx-auto px-4">
+          <div class="flex items-center justify-between">
+            <NuxtLink to="/" class="text-2xl font-bold font-serif">
+              ma.
+            </NuxtLink>
+            <div class="flex items-center gap-4 font-serif">
+              <NuxtLink to="/work"> Work </NuxtLink>
+              <NuxtLink to="/work"> About </NuxtLink>
+              <NuxtLink to="/work"> Contact </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="max-w-4xl mx-auto px-4 flex-1 flex flex-col">
+        <div
+          class="grid grid-cols-1 md:grid-cols-2 gap-8 py-24 place-items-center flex-1"
+        >
+          <div class="flex flex-col gap-8">
+            <h1 class="text-6xl font-serif">
+              Hi I'm <br />
+              <span class="font-semibold text-black">Misbah Ansori</span>
+            </h1>
+            <p class="text-gray-600 leading-loose">
+              I'm a full-stack developer with more that 4 years of experience in
+              web development. I'm passionate about learning new technologies
+              and building cool projects.
+            </p>
+            <button class="">
+              <svg
+                class="w-8"
+                viewBox="0 0 34 39"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19.2187 0.181824H14.4176V29.2102L3.31959 18.1122L0.0326538 21.4361L16.8182 38.2216L33.6406 21.4361L30.2798 18.1122L19.2187 29.2102V0.181824Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Project />
   </div>
 </template>
---
+++ components/Project.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="bg-gray-200 py-12">
+    <div class="max-w-4xl mx-auto px-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="flex flex-col gap-3">
+          <span class="uppercase font-semibold text-sm tracking-wide">
+            01/Project name
+          </span>
+          <p class="text-gray-600 leading-loose">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quod voluptates voluptatem quos
+            exercitationem quas doloribus quidem voluptate.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1600132806608-231446b2e7af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2874&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
