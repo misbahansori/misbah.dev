fab28a55158aa242f54bcf3082c83e051bac5512
Misbah Ansori <misbah.ansori24@gmail.com>
2023-08-27 10:03:44 +0700

    wip

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="max-w-4xl mx-auto px-4">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 py-24">
-      <h1 class="text-4xl font-serif">Hi My name is Misbah ansori</h1>
-      <p class="text-gray-600 leading-relaxed">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 py-24">
+      <h1 class="text-6xl font-serif">
+        Hi I'm <br />
+        <span class="font-semibold text-black">Misbah Ansori</span>
+      </h1>
+      <p class="text-gray-600 leading-loose">
         I'm a full-stack developer with more that 4 years of experience in web
         development. I'm passionate about learning new technologies and building
         cool projects.
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,8 +2,8 @@
   <nav class="py-8">
     <div class="max-w-4xl mx-auto px-4">
       <div class="flex items-center justify-between">
-        <NuxtLink to="/"> ma. </NuxtLink>
-        <div class="flex items-center gap-4">
+        <NuxtLink to="/" class="text-2xl font-bold font-serif"> ma. </NuxtLink>
+        <div class="flex items-center gap-4 font-serif">
           <NuxtLink to="/work"> Work </NuxtLink>
           <NuxtLink to="/work"> About </NuxtLink>
           <NuxtLink to="/work"> Contact </NuxtLink>
