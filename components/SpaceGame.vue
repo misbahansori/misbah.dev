e1d2d6f5cfa7e230b25ef00b54ff698a47cd0f03
Misbah Ansori <misbah.ansori24@gmail.com>
2023-09-01 13:24:55 +0700

    Refactor

--- components/SpaceGame.vue
+++ components/SpaceGame.vue
@@ -1,63 +1,114 @@
 <script setup lang="ts">
-const state = ref<"idle" | "playing" | "gameover">("idle");
+interface Coordinate {
+  x: number;
+  y: number;
+}
 const BOARD_SIZE = 24;
-const character = ref({ x: 1, y: 20 });
-const enemies = shallowRef<
-  {
-    x: number;
-    y: number;
-  }[]
->([
-  { x: 8, y: 1 },
-  { x: 14, y: 1 },
-]);
-const projectiles = shallowRef<
-  {
-    x: number;
-    y: number;
-  }[]
->([]);
-
-const enemyProjectiles = shallowRef<
-  {
-    x: number;
-    y: number;
-  }[]
->([]);
-
-const { ready: readyToFire, start: startFiring } = useTimeout(500, {
-  controls: true,
+const gameState = ref<"idle" | "playing" | "gameover">("idle");
+const state = shallowRef<{
+  renderCount: number;
+  character: Coordinate;
+  enemies: Coordinate[];
+  projectiles: Coordinate[];
+  enemyProjectiles: Coordinate[];
+}>({
+  renderCount: 0,
+  character: { x: 1, y: 20 },
+  enemies: [
+    { x: 8, y: 1 },
+    { x: 14, y: 1 },
+  ],
+  projectiles: [],
+  enemyProjectiles: [],
 });
+const { resume, pause } = useIntervalFn(
+  () => {
+    moveProjectiles();
 
-const { ready: readyToSpawnEnemy, start: resetSpawnEnemy } = useTimeout(3000, {
-  controls: true,
-});
+    if (state.value.renderCount % 20 === 0) {
+      spawnEnemy();
+
+      if (state.value.enemies.length) {
+        state.value.enemies.forEach((enemy) => {
+          if (Math.random() > 0.3) return;
+
+          enemyFire(enemy);
+        });
+      }
+    }
+
+    state.value.renderCount++;
+    triggerRef(state);
+  },
+  100,
+  { immediate: false }
+);
+
+const startGame = () => {
+  gameState.value = "playing";
+  resume();
+};
+
+const moveProjectiles = () => {
+  if (state.value.projectiles.length) {
+    state.value.projectiles = state.value.projectiles.filter((projectile) => {
+      projectile.y--;
+
+      if (projectileHitsEnemy(projectile)) {
+        killEnemy(projectile);
+        return false;
+      }
+
+      return projectile.y >= 0;
+    });
+  }
+  if (state.value.enemyProjectiles.length) {
+    state.value.enemyProjectiles = state.value.enemyProjectiles.filter(
+      (projectile) => {
+        projectile.y++;
+
+        if (
+          (projectile.x === state.value.character.x &&
+            projectile.y === state.value.character.y) ||
+          (projectile.x - 1 === state.value.character.x &&
+            projectile.y === state.value.character.y + 1) ||
+          (projectile.x + 1 === state.value.character.x &&
+            projectile.y === state.value.character.y + 1)
+        ) {
+          gameState.value = "gameover";
+        }
+
+        return projectile.y <= BOARD_SIZE;
+      }
+    );
+  }
+};
 
 const reset = () => {
-  state.value = "idle";
-  resetSpawnEnemy();
-  character.value = { x: 1, y: 20 };
-  enemies.value = [
+  gameState.value = "idle";
+  reset();
+  state.value.character = { x: 1, y: 20 };
+  state.value.enemies = [
     { x: 8, y: 1 },
     { x: 14, y: 1 },
   ];
-  projectiles.value = [];
-  enemyProjectiles.value = [];
+  state.value.projectiles = [];
+  state.value.enemyProjectiles = [];
 };
 
 const spawnEnemy = () => {
-  if (enemies.value.length >= 10) return;
-
-  resetSpawnEnemy();
+  if (state.value.enemies.length >= 10) return;
 
   // find a random x position that is not occupied by an enemy
   let x = Math.floor(Math.random() * BOARD_SIZE);
 
-  while (enemies.value.some((enemy) => enemy.x <= x - 3 && enemy.x >= x + 3)) {
+  while (
+    state.value.enemies.some((enemy) => enemy.x <= x - 3 && enemy.x >= x + 3)
+  ) {
     x = Math.floor(Math.random() * BOARD_SIZE);
   }
 
-  enemies.value.push({
+  state.value.enemies.push({
     x,
     y: 1 + Math.floor(Math.random() * 5),
   });
@@ -85,54 +136,51 @@ onKeyStroke("ArrowRight", (e) => {
 });
 
 const move = (direction: "left" | "right" | "up" | "down") => {
-  if (state.value !== "playing") return;
+  if (gameState.value !== "playing") return;
 
   if (direction === "left") {
-    if (character.value.x <= 0 + 1) return;
-    character.value.x--;
+    if (state.value.character.x <= 0 + 1) return;
+    state.value.character.x--;
   }
 
   if (direction === "right") {
-    if (character.value.x >= BOARD_SIZE - 2) return;
-    character.value.x++;
+    if (state.value.character.x >= BOARD_SIZE - 2) return;
+    state.value.character.x++;
   }
 
   if (direction === "up") {
-    if (character.value.y <= 0) return;
-    character.value.y--;
+    if (state.value.character.y <= 0) return;
+    state.value.character.y--;
   }
 
   if (direction === "down") {
-    if (character.value.y >= BOARD_SIZE - 2) return;
-    character.value.y++;
+    if (state.value.character.y >= BOARD_SIZE - 2) return;
+    state.value.character.y++;
   }
 };
 
 onKeyStroke(" ", (e) => {
   e.preventDefault();
-  state.value = "playing";
+  gameState.value = "playing";
   fire();
 });
 
 const fire = () => {
-  if (!readyToFire.value) return;
-
-  startFiring();
-  projectiles.value.push({
-    x: character.value.x,
-    y: character.value.y - 1,
+  state.value.projectiles.push({
+    x: state.value.character.x,
+    y: state.value.character.y - 1,
   });
 };
 
 const enemyFire = (enemy: { x: number; y: number }) => {
-  enemyProjectiles.value.push({
+  state.value.enemyProjectiles.push({
     x: enemy.x,
     y: enemy.y + 1,
   });
 };
 
 const projectileHitsEnemy = (projectile: { x: number; y: number }): boolean => {
-  return enemies.value.some(
+  return state.value.enemies.some(
     (enemy) =>
       (enemy.x === projectile.x && enemy.y === projectile.y) ||
       (enemy.x - 1 === projectile.x && enemy.y - 1 === projectile.y) ||
@@ -141,7 +189,7 @@ const projectileHitsEnemy = (projectile: { x: number; y: number }): boolean => {
 };
 
 const killEnemy = (projectile: { x: number; y: number }) => {
-  enemies.value = enemies.value.filter(
+  state.value.enemies = state.value.enemies.filter(
     (enemy) =>
       !(
         (enemy.x === projectile.x && enemy.y === projectile.y) ||
@@ -150,86 +198,20 @@ const killEnemy = (projectile: { x: number; y: number }) => {
       )
   );
 };
-
-watch(readyToSpawnEnemy, (ready) => {
-  if (state.value !== "playing") return;
-
-  if (!ready) return;
-
-  spawnEnemy();
-});
-
-watch(enemies, (enemies) => {
-  if (state.value !== "playing") return;
-
-  if (!enemies.length) {
-    nextTick(() => {
-      alert("You win!");
-    });
-  }
-});
-
-useIntervalFn(() => {
-  if (state.value !== "playing") return;
-
-  if (projectiles.value.length) {
-    projectiles.value = projectiles.value.filter((projectile) => {
-      projectile.y--;
-
-      if (projectileHitsEnemy(projectile)) {
-        killEnemy(projectile);
-        return false;
-      }
-
-      return projectile.y >= 0;
-    });
-  }
-
-  if (enemyProjectiles.value.length) {
-    enemyProjectiles.value = enemyProjectiles.value.filter((projectile) => {
-      projectile.y++;
-
-      if (
-        (projectile.x === character.value.x &&
-          projectile.y === character.value.y) ||
-        (projectile.x - 1 === character.value.x &&
-          projectile.y === character.value.y + 1) ||
-        (projectile.x + 1 === character.value.x &&
-          projectile.y === character.value.y + 1)
-      ) {
-        state.value = "gameover";
-      }
-
-      return projectile.y <= BOARD_SIZE;
-    });
-  }
-}, 50);
-
-useIntervalFn(() => {
-  if (state.value !== "playing") return;
-
-  if (enemies.value.length) {
-    enemies.value.forEach((enemy) => {
-      if (Math.random() > 0.3) return;
-
-      enemyFire(enemy);
-    });
-  }
-}, 1000);
 </script>
 
 <template>
   <div class="h-full w-full relative">
     <div class="inset-0 absolute flex items-center justify-center">
       <button
-        v-if="state === 'idle'"
+        v-if="gameState === 'idle'"
         class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
-        @click="state = 'playing'"
+        @click="startGame"
       >
         Start
       </button>
       <div
-        v-else-if="state === 'gameover'"
+        v-else-if="gameState === 'gameover'"
         class="flex flex-col items-center gap-2"
       >
         <div class="bg-gray-100 px-6 py-4">
@@ -237,7 +219,6 @@ useIntervalFn(() => {
         </div>
         <button
           class="relative z-10 block bg-gray-100 px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
-          @click="reset"
         >
           Reset
         </button>
@@ -247,7 +228,6 @@ useIntervalFn(() => {
       class="absolute inset-0 flex items-end justify-between md:hidden translate-y-10"
     >
       <button
-        @click="move('left')"
         class="relative z-10 block px-2 py-2 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
       >
         <svg
@@ -267,7 +247,6 @@ useIntervalFn(() => {
         </svg>
       </button>
       <button
-        @click="fire"
         class="relative z-10 block px-2 py-2 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
       >
         <svg
@@ -287,7 +266,6 @@ useIntervalFn(() => {
         </svg>
       </button>
       <button
-        @click="move('right')"
         class="relative z-10 block px-2 py-2 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
       >
         <svg
@@ -311,21 +289,21 @@ useIntervalFn(() => {
       class="w-full h-full aspect-square"
       :viewbox="`0 0 ${BOARD_SIZE} ${BOARD_SIZE}`"
     >
-      <Character :x="character.x" :y="character.y" />
+      <Character :x="state.character.x" :y="state.character.y" />
       <Enemy
-        v-for="enemy in enemies"
+        v-for="enemy in state.enemies"
         :key="`enemy-${enemy.x}-${enemy.y}`"
         :x="enemy.x"
         :y="enemy.y"
       />
       <Projectile
-        v-for="projectile in projectiles"
+        v-for="projectile in state.projectiles"
         :key="`projectile-${projectile.x}-${projectile.y}`"
         :x="projectile.x"
         :y="projectile.y"
       />
       <Projectile
-        v-for="projectile in enemyProjectiles"
+        v-for="projectile in state.enemyProjectiles"
         :key="`enemy-projectile-${projectile.x}-${projectile.y}`"
         :x="projectile.x"
         :y="projectile.y"
