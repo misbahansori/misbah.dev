13a2231001c236bca347bb331262ccc7dea8ec4d
Misbah Ansori <misbah.ansori24@gmail.com>
2023-09-10 10:01:59 +0700

    Feat : install prettier plugin tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -26,59 +26,59 @@ const links = [
 </script>
 
 <template>
-  <div class="bg-gray-100 text-gray-900 font-sans">
+  <div class="bg-gray-100 font-sans text-gray-900">
     <div class="min-h-screen">
       <aside
         style="writing-mode: vertical-lr"
-        class="bg-gray-950 w-16 md:w-24 fixed left-0 inset-y-0 flex-shrink-0 flex items-center gap-12 justify-start py-20 px-4 rotate-180"
+        class="fixed inset-y-0 left-0 flex w-16 flex-shrink-0 rotate-180 items-center justify-start gap-12 bg-gray-950 px-4 py-20 md:w-24"
       >
         <NuxtLink
           v-for="link in links"
           :to="link.url"
-          class="inline-blocK text-white uppercase text-sm whitespace-nowrap"
+          class="inline-blocK whitespace-nowrap text-sm uppercase text-white"
         >
           {{ link.name }}
         </NuxtLink>
       </aside>
-      <div class="flex flex-col px-8 lg:px-24 ml-16 md:ml-24">
-        <nav class="py-8 border-b border-gray-950/30 border-dashed">
+      <div class="ml-16 flex flex-col px-8 md:ml-24 lg:px-24">
+        <nav class="border-b border-dashed border-gray-950/30 py-8">
           <div class="flex items-center justify-between">
-            <NuxtLink to="/" class="text-3xl font-bold font-serif">
+            <NuxtLink to="/" class="font-serif text-3xl font-bold">
               ma.
             </NuxtLink>
-            <div class="md:items-center md:gap-12 font-serif hidden md:flex">
+            <div class="hidden font-serif md:flex md:items-center md:gap-12">
               <NuxtLink
-                to="/work"
-                class="font-medium tracking-wide uppercase text-gray-700 hover:text-gray-900"
+                to="/"
+                class="font-medium uppercase tracking-wide text-gray-700 hover:text-gray-900"
               >
                 Work
               </NuxtLink>
               <NuxtLink
-                to="/work"
-                class="font-medium tracking-wide uppercase text-gray-700 hover:text-gray-900"
+                to="/about"
+                class="font-medium uppercase tracking-wide text-gray-700 hover:text-gray-900"
               >
                 About
               </NuxtLink>
               <NuxtLink
-                to="/work"
-                class="font-medium tracking-wide uppercase text-gray-700 hover:text-gray-900"
+                to="/"
+                class="font-medium uppercase tracking-wide text-gray-700 hover:text-gray-900"
               >
                 Contact
               </NuxtLink>
             </div>
           </div>
         </nav>
-        <div class="flex-1 flex flex-col">
+        <div class="flex flex-1 flex-col">
           <div
-            class="grid grid-cols-1 md:grid-cols-2 gap-8 pt-12 lg:gap-24 pb-16 md:pt-20 md:pb-24 place-items-center flex-1"
+            class="grid flex-1 grid-cols-1 place-items-center gap-8 pb-16 pt-12 md:grid-cols-2 md:pb-24 md:pt-20 lg:gap-24"
           >
             <div class="flex flex-col gap-8">
-              <h1 class="text-5xl/tight md:text-7xl/tight font-serif">
+              <h1 class="font-serif text-5xl/tight md:text-7xl/tight">
                 <span class="text-3xl md:text-7xl">Hi I'm</span>
                 <br />
                 <span class="font-extrabold text-black">Misbah Ansori</span>
               </h1>
-              <p class="text-gray-600 leading-loose text-sm md:text-base">
+              <p class="text-sm leading-loose text-gray-600 md:text-base">
                 I'm a full-stack developer with more that 4 years of experience
                 in web development. I'm passionate about learning new
                 technologies and building cool projects.
@@ -86,31 +86,31 @@ const links = [
               <div>
                 <NuxtLink
                   to="https://github.com/misbahansori"
-                  class="relative inline-block text-lg group"
+                  class="group relative inline-block text-lg"
                 >
                   <span
-                    class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 group-hover:text-white"
+                    class="relative z-10 block overflow-hidden border-2 border-gray-900 px-5 py-3 font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out group-hover:text-white"
                   >
                     <span
-                      class="absolute inset-0 w-full h-full px-5 py-3 bg-gray-50"
+                      class="absolute inset-0 h-full w-full bg-gray-50 px-5 py-3"
                     ></span>
                     <span
-                      class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"
+                      class="ease absolute left-0 -ml-2 h-48 w-48 origin-top-right -translate-x-full translate-y-12 -rotate-90 bg-gray-900 transition-all duration-300 group-hover:-rotate-180"
                     ></span>
                     <span
-                      class="relative text-sm uppercase font-bold tracking-wide"
+                      class="relative text-sm font-bold uppercase tracking-wide"
                     >
                       View my profile
                     </span>
                   </span>
                   <span
-                    class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 group-hover:mb-0 group-hover:mr-0"
+                    class="absolute bottom-0 right-0 -mb-1 -mr-1 h-12 w-full bg-gray-900 transition-all duration-200 ease-linear group-hover:mb-0 group-hover:mr-0"
                   ></span>
                 </NuxtLink>
               </div>
             </div>
 
-            <SpaceGame />
+            <!-- <SpaceGame /> -->
           </div>
         </div>
       </div>
