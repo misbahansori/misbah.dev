2d8720222327a8ec65d132ba584ac90c285ac557
Misbah Ansori <misbah.ansori24@gmail.com>
2024-01-17 19:40:11 +0700

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { ArrowTopRightIcon, MoonIcon, SunIcon } from "@radix-icons/vue";
+import {
+  ArrowTopRightIcon,
+  EnvelopeOpenIcon,
+  MoonIcon,
+  SunIcon,
+} from "@radix-icons/vue";
 const skills = [
   {
     name: "Laravel",
@@ -32,6 +37,9 @@ const colorMode = useColorMode();
 </script>
 <template>
   <div class="min-h-screen">
+    <div
+      class="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"
+    ></div>
     <section class="mx-auto max-w-3xl px-4">
       <div class="flex flex-col gap-4 pb-6 pt-12">
         <img src="/img/misbah.jpg" alt="" class="h-20 w-20 rounded-full" />
@@ -44,16 +52,24 @@ const colorMode = useColorMode();
               easier
             </p>
           </div>
-          <div>
-            <Button
-              v-if="colorMode.value === 'dark'"
-              @click="colorMode.value = 'light'"
-              class="h-auto p-2"
-            >
-              <SunIcon class="h-5 w-5" />
+          <div class="flex items-center gap-3">
+            <Button class="h-auto p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                class="h-5 w-5"
+              >
+                <path
+                  fill="currentColor"
+                  d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
+                />
+              </svg>
             </Button>
-            <Button v-else @click="colorMode.value = 'dark'" class="h-auto p-2">
-              <MoonIcon class="h-5 w-5" />
+            <Button class="flex gap-3">
+              <EnvelopeOpenIcon class="h-4 w-4" />
+              <span>Get in touch </span>
             </Button>
           </div>
         </div>
@@ -87,5 +103,17 @@ const colorMode = useColorMode();
         </div>
       </div>
     </section>
+    <div class="fixed bottom-4 right-4">
+      <Button
+        v-if="colorMode.value === 'dark'"
+        @click="colorMode.value = 'light'"
+        class="h-auto p-2"
+      >
+        <SunIcon class="h-5 w-5" />
+      </Button>
+      <Button v-else @click="colorMode.value = 'dark'" class="h-auto p-2">
+        <MoonIcon class="h-5 w-5" />
+      </Button>
+    </div>
   </div>
 </template>
