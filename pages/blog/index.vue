1630d9e1a25ea76ba8e18d316b6155ccf5957bf7
Misbah Ansori <misbah.ansori24@gmail.com>
2024-01-20 08:29:43 +0700

    wip

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,13 +5,35 @@
       <p class="text-muted-foreground">Just some random thoughts of mine</p>
       <div class="pt-12">
         <ContentList path="/blog" v-slot="{ list }">
-          <NuxtLink
-            v-for="article in list"
-            :key="article._path"
-            :to="article._path"
-          >
-            <h2>{{ article.title }}</h2>
-          </NuxtLink>
+          <div v-for="article in list" :key="article._path">
+            <article class="group relative">
+              <div
+                class="absolute -inset-x-4 -inset-y-2.5 transition group-hover:bg-accent-foreground/5 group-hover:backdrop-blur-[2px] sm:rounded-2xl md:-inset-x-6 md:-inset-y-4"
+              ></div>
+              <div class="relative">
+                <h3 class="pt-8 text-base font-semibold tracking-tight lg:pt-0">
+                  {{ article.title }}
+                </h3>
+                <div class="mb-4 mt-2 line-clamp-2">
+                  <p>
+                    {{ article.description }}
+                  </p>
+                </div>
+              </div>
+              <NuxtLink
+                class="flex items-center text-sm font-medium text-sky-500"
+                :to="article._path"
+              >
+                <span
+                  class="absolute -inset-x-4 -inset-y-2.5 sm:rounded-2xl md:-inset-x-6 md:-inset-y-4"
+                ></span>
+                <span class="relative">
+                  Read more
+                  <span class="sr-only">, {{ article.title }}</span>
+                </span>
+              </NuxtLink>
+            </article>
+          </div>
         </ContentList>
       </div>
     </div>
