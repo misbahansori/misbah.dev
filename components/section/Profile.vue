6b1b914ba0eafde2eadd6099710c9a75b8fdbad4
Misbah Ansori <misbah.ansori24@gmail.com>
2024-01-25 10:42:20 +0700

    Fix

--- components/section/Profile.vue
+++ components/section/Profile.vue
@@ -28,7 +28,7 @@ import { buttonVariants } from "~/components/ui/button";
                 <TooltipContent>
                   <div
                     v-motion="{
-                      initial: { opacity: 0, y: 20, scale: 0.6, rotate: -10 },
+                      initial: { opacity: 0, y: 20, scale: 0.6, rotate: -20 },
                       enter: {
                         opacity: 1,
                         y: 0,
@@ -46,14 +46,41 @@ import { buttonVariants } from "~/components/ui/button";
                         transition: { duration: 0.1 },
                       },
                     }"
-                    class="pointer-events-none absolute flex flex-col items-center justify-center rounded-2xl bg-background px-6 py-2 shadow-xl"
+                    class="pointer-events-none absolute -translate-x-1/2 -translate-y-full"
                   >
-                    <span class="whitespace-nowrap font-semibold">
-                      Misbah Ansori
-                    </span>
-                    <span class="whitespace-nowrap text-sm">
-                      Fullstack Developer
-                    </span>
+                    <div class="-translate-x-1/2 -translate-y-full">
+                      <div
+                        class="flex flex-col items-center justify-center rounded-2xl bg-background px-6 py-2 shadow-xl"
+                      >
+                        <span class="whitespace-nowrap font-semibold">
+                          Misbah Ansori
+                        </span>
+                        <span class="whitespace-nowrap text-sm">
+                          Fullstack Developer
+                        </span>
+                      </div>
+                      <div class="relative h-5 w-full overflow-hidden">
+                        <div
+                          class="absolute inset-x-1/2 top-0 h-[2px] w-full -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent blur-sm"
+                        ></div>
+                        <div
+                          class="absolute inset-x-1/2 top-0 h-px w-full -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent"
+                        ></div>
+                        <div
+                          class="absolute inset-x-1/2 top-0 h-[4px] w-1/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent blur-sm"
+                        ></div>
+                        <div
+                          class="absolute inset-x-1/2 top-0 h-px w-1/4 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent"
+                        ></div>
+                        <div
+                          class="h-full w-full opacity-0"
+                          style="opacity: 1"
+                        ></div>
+                        <div
+                          class="absolute inset-0 h-full w-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"
+                        ></div>
+                      </div>
+                    </div>
                   </div>
                 </TooltipContent>
               </TooltipPortal>
@@ -65,15 +92,6 @@ import { buttonVariants } from "~/components/ui/button";
       <div
         class="flex flex-col justify-between gap-8 sm:flex-row sm:items-start"
       >
-        <div>
-          <h1 class="text-2xl font-bold">Hi, I'm Misbah Ansori</h1>
-          <span class="text-base font-medium">Fullstack Developer</span>
-          <p class="pt-2.5 text-muted-foreground">
-            A passionate developer who has a strong interest in web development
-            and technology. Proficient and experienced in both frontend and
-            backend.
-          </p>
-        </div>
         <div class="flex items-center gap-3">
           <NuxtLink
             to="https://twitter.com/MisbahAnsori24"
