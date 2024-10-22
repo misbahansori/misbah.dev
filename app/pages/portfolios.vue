380e5cfbccd49de0dff5c3013b2e3b8f4200b140
Misbah Ansori <misbah.ansori24@gmail.com>
2024-10-22 08:37:53 +0700

    Feat : add portfolio

--- app/pages/portfolios.vue
+++ app/pages/portfolios.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ExternalLinkIcon } from "@radix-icons/vue";
+
 useHead({
   title: "My portfolios",
 });
@@ -8,6 +10,24 @@ useSeoMeta({
   ogTitle: "My portfolios",
   ogDescription: "Useful links that I often visit and use",
 });
+
+const portfolios = ref([
+  {
+    title: "ShareShooter",
+    imageUrl: {
+      light: "/img/shareshooter-light.jpeg",
+      dark: "/img/shareshooter-dark.jpeg",
+    },
+    description: "Easily share screenshots with your friends",
+    link: "https://www.shareshooter.com",
+  },
+]);
+
+const colorMode = useColorMode();
+
+watch(colorMode, () => {
+  console.log("Color mode changed to", colorMode.value);
+});
 </script>
 
 <template>
@@ -15,28 +35,40 @@ useSeoMeta({
     <div class="flex flex-col gap-4 py-6 lg:py-8">
       <h1 class="text-xl font-bold">My Portfolios</h1>
       <p class="text-muted-foreground">Here are some of my portfolios</p>
-      <div class="pt-8">
-        <div class="flex flex-col items-center justify-center gap-4">
+      <div class="grid grid-cols-1 pt-8 sm:grid-cols-2">
+        <div
+          v-for="portfolio in portfolios"
+          :key="portfolio.title"
+          class="group relative flex flex-col gap-4 rounded-xl border bg-background p-5 shadow-sm"
+        >
           <div>
-            <Icon
-              name="my-icon:task-empty"
-              mode="svg"
-              class="mx-auto mt-8 size-40 animate-hover text-muted-foreground"
-              :font-controlled="false"
-            />
-
-            <Icon
-              name="my-icon:empty-shadow"
-              mode="svg"
-              class="mx-auto h-12 w-40 animate-hover-shadow text-muted-foreground"
-              :font-controlled="false"
+            <img
+              :src="
+                portfolio.imageUrl[
+                  colorMode.value === 'dark' ? 'dark' : 'light'
+                ]
+              "
+              alt="Portfolio image"
+              class="w-full rounded-lg"
             />
           </div>
-
-          <p class="max-w-sm text-center italic leading-loose">
-            Just an empty page for now. Hopefully I can show something here
-            soon.
-          </p>
+          <div class="relative flex flex-col gap-1">
+            <h2 class="text-xl font-bold">{{ portfolio.title }}</h2>
+            <p class="text-muted-foreground">{{ portfolio.description }}</p>
+            <NuxtLink
+              :href="portfolio.link"
+              target="_blank"
+              external
+              class="text-indigo-500 hover:underline"
+            >
+              {{ portfolio.link }}
+            </NuxtLink>
+            <NuxtLink :to="portfolio.link" target="_blank" external>
+              <ExternalLinkIcon
+                class="absolute right-2 top-2 h-4 w-4 origin-bottom-left scale-75 opacity-0 transition duration-150 group-hover:scale-100 group-hover:opacity-90"
+              />
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
