1e61138f7961bbcf83b2bd682a83c4909a7c653c
Misbah Ansori <misbah.ansori24@gmail.com>
2025-02-22 17:33:44 +0700

    fix

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -16,7 +16,7 @@ if (!article.value) {
   <section class="mx-auto max-w-3xl px-4">
     <div class="flex flex-col gap-4 py-6 lg:py-8">
       <div
-        class="prose-sm dark:prose-invert md:prose prose-pre:bg-muted prose-pre:p-4 prose-hr:my-8"
+        class="prose prose-sm dark:prose-invert md:prose-base prose-pre:bg-muted prose-pre:p-4 prose-hr:my-8"
       >
         <ContentRenderer v-if="article" :value="article" />
       </div>
