75f8ab749c11c565db0edabacd79e54b9380a035
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-19 14:53:20 +0700

    wip

--- app/components/section/Portfolio.vue
+++ app/components/section/Portfolio.vue
@@ -32,10 +32,10 @@ const portfolios = [
         <div
           v-for="portfolio in portfolios"
           :key="portfolio.title"
-          class="bg-muted dark:border-border relative flex rounded-3xl border border-zinc-300 p-6 shadow-sm ring-0 inset-shadow-2xs shadow-zinc-950/10 inset-shadow-white duration-150 dark:inset-shadow-transparent"
+          class="bg-muted dark:border-border group relative flex overflow-hidden rounded-3xl border border-zinc-300 px-6 pt-6 shadow-sm ring-0 inset-shadow-2xs shadow-zinc-950/10 inset-shadow-white duration-150 dark:inset-shadow-transparent"
         >
           <div
-            class="border-border aspect-[4/3] overflow-hidden rounded-xl border shadow-xl"
+            class="border-border -mb-1 aspect-[4/3] origin-bottom overflow-hidden rounded-t-xl border shadow-xl transition group-hover:scale-105"
           >
             <img
               :src="portfolio.image.dark"
