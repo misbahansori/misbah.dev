b59f3706f2f25f3b37dfacc3f35879c3636a89e8
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-19 15:01:54 +0700

    wip

--- app/components/section/Portfolio.vue
+++ app/components/section/Portfolio.vue
@@ -25,28 +25,38 @@ const portfolios = [
   <section>
     <div class="relative mx-auto max-w-3xl px-4 py-24">
       <BorderX />
-      <div class="flex flex-col gap-4">
-        <h2 class="text-2xl/snug">My Projects</h2>
-      </div>
-      <div class="grid grid-cols-2 gap-8 py-12">
-        <div
-          v-for="portfolio in portfolios"
-          :key="portfolio.title"
-          class="bg-muted dark:border-border group relative flex overflow-hidden rounded-3xl border border-zinc-300 px-6 pt-6 shadow-sm ring-0 inset-shadow-2xs shadow-zinc-950/10 inset-shadow-white duration-150 dark:inset-shadow-transparent"
-        >
+      <div class="relative">
+        <div class="flex flex-col gap-4">
+          <h2 class="text-2xl/snug">My Projects</h2>
+        </div>
+        <div class="grid grid-cols-2 gap-8 py-12">
           <div
-            class="border-border -mb-1 aspect-[4/3] origin-bottom overflow-hidden rounded-t-xl border shadow-xl transition group-hover:scale-105"
+            v-for="portfolio in portfolios"
+            :key="portfolio.title"
+            class="flex flex-col gap-4"
           >
-            <img
-              :src="portfolio.image.dark"
-              alt="OriginUi Vue"
-              class="hidden h-full w-full object-cover dark:block"
-            />
-            <img
-              :src="portfolio.image.light"
-              alt="OriginUi Vue"
-              class="h-full w-full object-cover dark:hidden"
-            />
+            <div
+              class="bg-muted dark:border-border group relative flex overflow-hidden rounded-3xl border border-zinc-300 px-6 pt-6 shadow-sm ring-0 inset-shadow-2xs shadow-zinc-950/10 inset-shadow-white duration-150 dark:inset-shadow-transparent"
+            >
+              <div
+                class="border-border -mb-1 aspect-[4/3] origin-bottom overflow-hidden rounded-t-xl border shadow-xl transition group-hover:scale-105"
+              >
+                <img
+                  :src="portfolio.image.dark"
+                  alt="OriginUi Vue"
+                  class="hidden h-full w-full object-cover dark:block"
+                />
+                <img
+                  :src="portfolio.image.light"
+                  alt="OriginUi Vue"
+                  class="h-full w-full object-cover dark:hidden"
+                />
+              </div>
+            </div>
+            <div class="flex flex-col gap-2">
+              <h3 class="text-lg font-medium">{{ portfolio.title }}</h3>
+              <p class="text-sm text-zinc-500">{{ portfolio.description }}</p>
+            </div>
           </div>
         </div>
       </div>
--- app/components/section/Profile.vue
+++ app/components/section/Profile.vue
@@ -28,28 +28,36 @@ const showTooltip = ref(false);
     <div
       class="border-y bg-[image:repeating-linear-gradient(315deg,_var(--accent)_0,_var(--accent)_1px,_transparent_0,_transparent_50%)] bg-[size:15px_15px] bg-fixed"
     >
-      <div class="relative mx-auto max-w-3xl px-4 py-4">
+      <div class="relative mx-auto max-w-3xl px-4">
         <BorderX />
 
-        <div class="relative flex gap-2">
-          <Button size="lg" class="group rounded-full">
-            <span>View profile</span>
-            <Icon
-              name="lucide:arrow-right"
-              class="size-4 transition group-hover:translate-x-1"
-            />
-          </Button>
-          <Button size="lg" variant="secondary" class="rounded-full">
-            <span class="relative flex size-3">
-              <span
-                class="bg-secondary-foreground/80 absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"
-              ></span>
-              <span
-                class="bg-secondary-foreground relative inline-flex size-3 rounded-full"
-              ></span>
-            </span>
-            Available for hire
-          </Button>
+        <div class="flex">
+          <div class="bg-background relative flex gap-2">
+            <div class="bg-background absolute -inset-x-4 inset-y-0 border-x" />
+            <div class="relative py-4">
+              <Button size="lg" class="group rounded-full">
+                <span>View profile</span>
+                <Icon
+                  name="lucide:arrow-right"
+                  class="size-4 transition group-hover:translate-x-1"
+                />
+              </Button>
+            </div>
+            <div class="bg-border relative h-full w-px" />
+            <div class="relative py-4">
+              <Button size="lg" variant="secondary" class="rounded-full">
+                <span class="relative flex size-3">
+                  <span
+                    class="bg-secondary-foreground/80 absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"
+                  ></span>
+                  <span
+                    class="bg-secondary-foreground relative inline-flex size-3 rounded-full"
+                  ></span>
+                </span>
+                Available for hire
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
